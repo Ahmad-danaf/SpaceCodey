181669e7d5c0514d60b1d2815cfb08ba404c5e6e
Ahmad Danaf <ahmad.danf@gmail.com>
2024-06-12 13:51:32 +0300

    Replaced calculate_optimal_shooting_times with best_astrophotography_times

--- AstroHub/events/urls.py
+++ AstroHub/events/urls.py
@@ -6,8 +6,7 @@ app_name='events'
 urlpatterns = [
     path('events/', views.event_list, name='event_list'),
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
-    path('optimal_times/', views.calculate_optimal_times, name='optimal_times'),
     path('body_info/', views.body_info, name='body_info'),
-    path('input_location/', views.input_location, name='input_location'),
+    path('optimal_times_form/', views.input_location, name='optimal_times_form'),
     path('display_optimal_times/', views.display_optimal_times, name='display_optimal_times'),
 ]
